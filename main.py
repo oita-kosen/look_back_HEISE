e4d67d72889a23274c93051741e87c898ce4820d
yamamotsu <motsulab@gmail.com>
2019-04-22 21:57:45 +0900

    [add] monkeypatch

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
+import eventlet
 from flask import Flask, render_template
 from flask_socketio import SocketIO, emit
 from flask_mqtt import Mqtt
 import requests
+
+eventlet.monkey_patch()
 
 app = Flask(__name__)
 app.config['MQTT_BROKER_URL'] = 'm16.cloudmqtt.com'
