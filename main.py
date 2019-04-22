7e9311c02916aa33030fcf76635abdc96f630da3
yamamotsu <motsulab@gmail.com>
2019-04-22 22:10:01 +0900

    [fix] news pick by mqtt

--- main.py
+++ main.py
@@ -48,11 +48,12 @@ def handle_mqtt_message(client, userdata, message):
     #     topic=message.topic,
     #     payload=message.payload.decode()
     # )
-    # response_news = requests.get(url_news)
-    # data = response_news.json()
 
-    socketio.emit('my_content', {'title': 'MQTT', 'url': 'mqtt','date': 'mqtt', 'img': 'mqtt','genre': 'mqtt'}, namespace='/test')
-    #socketio.emit('my_content', {'title': data['title'], 'url': data['url'],'date': data['date'], 'img': data['img'],'genre': data['genre']}, namespace='/test')
+    response_news = requests.get(url_news)
+    data = response_news.json()
+
+    #socketio.emit('my_content', {'title': 'MQTT', 'url': 'mqtt','date': 'mqtt', 'img': 'mqtt','genre': 'mqtt'}, namespace='/test')
+    socketio.emit('my_content', {'title': data['title'], 'url': data['url'],'date': data['date'], 'img': data['img'],'genre': data['genre']+' by mqtt'}, namespace='/test')
     mqtt.publish('log', 'emit!')
 
 if __name__ == '__main__':
