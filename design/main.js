8e5dbdc7f3f6bfa240a797596036d2775a02497e
yuma yuma <yumayuma0322@gmail.com>
2019-04-27 00:02:36 +0900

    add design page

--- design/main.js
+++ design/main.js
@@ -1,14 +1,40 @@
 $(document).ready(function() {
 
-    //htmlのフォームがsubmitされた時に、main.pyのreceive_content宛にテキストエリアのid="input_data"の値を送信します。
-    $('form#broadcast').submit(function(event) {
-        var title = $('#data').val();
-        var count = 30;
-        var length = title.length;
-        if(length > count){
-            title = title.substring(0,count);
-            title += '...';
-        }
-        console.log(title);
-    });
+	//htmlのフォームがsubmitされた時に、main.pyのreceive_content宛にテキストエリアのid="input_data"の値を送信します。
+	var count = 0;
+	var maxcards = 6;
+	$('form#broadcast').submit(function(event) {
+		var arr = $('.card').length;
+		console.log(arr);
+		count++;
+        $(".row").prepend('<div class="col-xs-12 col-sm-4"><div class="card"><a class="img-card" href="https://ja.wikipedia.org/wiki/%E5%B9%B3%E6%88%90"><img src="./images/heisei.jpg" /></a><div class="card-content"><h4 class="card-title"><a href="https://ja.wikipedia.org/wiki/%E5%B9%B3%E6%88%90"> 平成の振り返り</a></h4><table  class="t12 font12"><tr><td class="table-title">ジャンル</td><td>：</td><td class="genre">'+count+'</td></tr><tr><td class="table-title">日付</td><td>：</td><td class="date">1998/03/22</td></tr></table></div><div class="card-read-more"><a href="https://ja.wikipedia.org/wiki/%E5%B9%B3%E6%88%90" class="btn btn-link btn-block">Read More</a></div></div></div>');
+
+		$('.card').each(function(){
+			var index =$('.card').index(this); //何番目か
+			if(index >= maxcards){
+				$(this).addClass('disnone');
+				$('.more').removeClass('disnone');
+			}
+		});
+	});
+	$('#more').click(function () { 
+		$('.card').each(function(){
+			var index =$('.card').index(this); //何番目か
+			if(index >= maxcards){
+				$(this).removeClass('disnone');
+				$('.more').addClass('disnone');
+				$('.cl').removeClass('disnone');
+			}
+		});
+	});
+	$('#close').click(function () { 
+		$('.card').each(function(){
+			var index =$('.card').index(this); //何番目か
+			if(index >= maxcards){
+				$(this).addClass('disnone');
+				$('.more').removeClass('disnone');
+				$('.cl').addClass('disnone');
+			}
+		});
+	});
 });
