5cdfabf1f9cae4d05e6b858b7c6dc965de34b6d1
中岡黎 <reideeplearning@gmail.com>
2019-04-28 00:01:35 +0900

    small fix

--- main.py
+++ main.py
@@ -102,25 +102,25 @@ def handle_mqtt_message(client, userdata, message):
 
     if message.payload.decode() == '+1':  #右向いた時（遅い時）
         data = response_twitter_1.json()
-        socketio.emit('my_content', {'title': data['title'], 'url': data['url'],'date': data['date'], 'img': data['img'],'genre': data['genre']+' by mqtt (R+1)'},
+        socketio.emit('my_content', {'title': data['title'], 'url': data['url'],'date': data['date'], 'img': data['img'],'genre': data['genre']+' by mqtt (R_slow)'},
                       namespace='/test')
         response_twitter_1 = requests.get(url_twitter_1)
 
     elif message.payload.decode() == '+2':   #右向いた時（早い時）
         data = response_twitter_2.json()
-        socketio.emit('my_content', {'title': data['title'], 'url': data['url'],'date': data['date'], 'img': data['img'],'genre': data['genre']+' by mqtt (R+2)'},
+        socketio.emit('my_content', {'title': data['title'], 'url': data['url'],'date': data['date'], 'img': data['img'],'genre': data['genre']+' by mqtt (R_fast)'},
                       namespace='/test')
         response_twitter_2 = requests.get(url_twitter_2)
 
     elif message.payload.decode() == '-1':   #左向いた時（遅い時）
         data = response_news_1.json()
-        socketio.emit('my_content', {'title': data['title'], 'url': data['url'],'date': data['date'], 'img': data['img'],'genre': data['genre']+' by mqtt (L-1)'},
+        socketio.emit('my_content', {'title': data['title'], 'url': data['url'],'date': data['date'], 'img': data['img'],'genre': data['genre']+' by mqtt (L_slow)'},
                       namespace='/test')
         response_news_1 = requests.get(url_news_1)
 
     elif message.payload.decode() == '-2':   #左向いた時（早い時）
         data = response_news_2.json()
-        socketio.emit('my_content', {'title': data['title'], 'url': data['url'],'date': data['date'], 'img': data['img'],'genre': data['genre']+' by mqtt (L-2)'},
+        socketio.emit('my_content', {'title': data['title'], 'url': data['url'],'date': data['date'], 'img': data['img'],'genre': data['genre']+' by mqtt (L_fast)'},
                       namespace='/test')
         response_news_2 = requests.get(url_news_2)
 
