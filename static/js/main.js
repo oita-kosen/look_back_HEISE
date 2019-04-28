658a147023e0ce64a2a7fe17b6a27224119b8d06
yuma yuma <yumayuma0322@gmail.com>
2019-04-29 01:14:32 +0900

    js & html ”これから”ジャンルの処理追加

--- static/js/main.js
+++ static/js/main.js
@@ -16,8 +16,11 @@ $(document).ready(function() {
         }
         PlaySound();
         if(msg.genre.match('twitter')){
-            $(".row").prepend('<div class="col-xs-12 col-sm-4"><div class="card twitter_card"><a class="img-card" href="'+ msg.url +'" target="_blank"><img src="'+msg.img+'" /></a><div class="card-content"><h4 class="card-title"><a href="'+ msg.url +'" target="_blank">' + title + '</a></h4><table  class="t12 font12"><tr><td class="table-title">ジャンル</td><td>：</td><td class="genre">'+ msg.genre +'</td></tr><tr><td class="table-title">日付</td><td>：</td><td class="date">'+ msg.date +'</td></tr></table></div><div class="card-read-more"><a href="'+ msg.url +'" class="btn btn-link btn-block" target="_blank">Read More</a></div></div></div>'); // <div id="place"></div>内に、受け取ったdataを挿入します。
-        }else{
+            $(".row").prepend('<div class="col-xs-12 col-sm-4"><div class="card twitter_card"><a class="img-card" href="'+ msg.url +'" target="_blank"><img src="'+msg.img+'" /></a><div class="card-content"><h4 class="card-title"><a href="'+ msg.url +'" target="_blank">' + title + '</a></h4><table  class="t12 font12"><tr><td class="table-title">ジャンル</td><td>：</td><td class="genre">'+ msg.genre +'</td></tr><tr><td class="table-title">日付</td><td>：</td><td class="date">'+ msg.date +'</td></tr></table></div><div class="card-read-more"><a href="'+ msg.url +'" class="btn btn-link btn-block" target="_blank">Read More</a></div></div></div>'); 
+        }else if(msg.genre == "これから"){
+            $(".row").prepend('<div class="col-xs-12 col-sm-4"><div class="card future_card"><a class="img-card" href="'+ msg.url +'" target="_blank"><img src="'+msg.img+'" /></a><div class="card-content"><h4 class="card-title"><a href="'+ msg.url +'" target="_blank">' + title + '</a></h4><table  class="t12 font12"><tr><td class="table-title">ジャンル</td><td>：</td><td class="genre">'+ msg.genre +'</td></tr><tr><td class="table-title">日付</td><td>：</td><td class="date">'+ msg.date +'</td></tr></table></div><div class="card-read-more"><a href="'+ msg.url +'" class="btn btn-link btn-block" target="_blank">Read More</a></div></div></div>'); 
+        }
+        else{
             $(".row").prepend('<div class="col-xs-12 col-sm-4"><div class="card"><a class="img-card" href="'+ msg.url +'" target="_blank"><img src="'+msg.img+'" /></a><div class="card-content"><h4 class="card-title"><a href="'+ msg.url +'" target="_blank">' + title + '</a></h4><table  class="t12 font12"><tr><td class="table-title">ジャンル</td><td>：</td><td class="genre">'+ msg.genre +'</td></tr><tr><td class="table-title">日付</td><td>：</td><td class="date">'+ msg.date +'</td></tr></table></div><div class="card-read-more"><a href="'+ msg.url +'" class="btn btn-link btn-block" target="_blank">Read More</a></div></div></div>'); 
         }
         $('.card').each(function(){
@@ -41,6 +44,13 @@ $(document).ready(function() {
     });
     $('form#twitter').submit(function(event) {
         socket.emit('my_broadcast_event',{event:"twitter"});
+        console.log("protocol" + ':' + location.protocol);
+        console.log("domain" + ':' + document.domain);
+        console.log("port" + ':' + location.port);
+        return false;
+    });
+    $('form#reiwa').submit(function(event) {
+        socket.emit('my_broadcast_event',{event:"reiwa"});
         console.log("protocol" + ':' + location.protocol);
         console.log("domain" + ':' + document.domain);
         console.log("port" + ':' + location.port);
